24438db4942d18efb5722c975d6d0d28008a1088
publicqi <qi@fuzz.land>
2024-11-06 01:32:06 +0800

    fix future version object

--- crates/sui-core/src/authority.rs
+++ crates/sui-core/src/authority.rs
@@ -450,7 +450,11 @@ impl ObjectCacheRead for InputLoaderCache<'_> {
     fn _get_live_objref(&self, object_id: ObjectID) -> SuiResult<ObjectRef> {
         for (cache_id, obj) in &self.cache {
             if cache_id == &object_id {
-                return Ok((object_id, obj.version().next(), obj.digest()));
+                return Ok((
+                    object_id,
+                    obj.version().one_before().unwrap_or_default(),
+                    obj.digest(),
+                ));
             }
         }
 
