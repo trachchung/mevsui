f3a676a90c00fd70723b289822d7ec26059f2dca
publicqi <qi@fuzz.land>
2024-11-05 23:19:47 +0800

    pub field

--- crates/sui-types/src/base_types.rs
+++ crates/sui-types/src/base_types.rs
@@ -210,7 +210,7 @@ pub type ConsensusObjectSequenceKey = (ObjectID, SequenceNumber);
 /// The inner representation is private to prevent incorrectly constructing an `Other` instead of
 /// one of the specialized variants, e.g. `Other(GasCoin::type_())` instead of `GasCoin`
 #[derive(Eq, PartialEq, PartialOrd, Ord, Debug, Clone, Deserialize, Serialize, Hash)]
-pub struct MoveObjectType(MoveObjectType_);
+pub struct MoveObjectType(pub MoveObjectType_);
 
 /// Even though it is declared public, it is the "private", internal representation for
 /// `MoveObjectType`
