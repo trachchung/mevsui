664ac06623cabe1d42b242719451fa9022ed4378
Jacob <61052369+jacob-chia@users.noreply.github.com>
2024-12-05 22:28:29 +0800

    dry_run support ipc (#3)

--- crates/sui-sdk/src/apis.rs
+++ crates/sui-sdk/src/apis.rs
@@ -654,14 +654,21 @@ impl ReadApi {
         tx: TransactionData,
         override_objects: Vec<(ObjectID, Object)>,
     ) -> SuiRpcResult<DryRunTransactionBlockResponse> {
-        Ok(self
-            .api
-            .http
-            .dry_run_transaction_block_override(
-                Base64::from_bytes(&bcs::to_bytes(&tx)?),
-                Base64::from_bytes(&bcs::to_bytes(&override_objects)?),
-            )
-            .await?)
+        let resp = if let Some(ref ipc) = self.api.ipc {
+            ipc.dry_run_transaction_block_override(tx, override_objects)
+                .await
+                .map_err(|e| Error::IpcError(e.to_string()))?
+        } else {
+            self.api
+                .http
+                .dry_run_transaction_block_override(
+                    Base64::from_bytes(&bcs::to_bytes(&tx)?),
+                    Base64::from_bytes(&bcs::to_bytes(&override_objects)?),
+                )
+                .await?
+        };
+
+        Ok(resp)
     }
 
     /// Return the inspection of the transaction block, or an error upon failure.
--- crates/sui-sdk/src/lib.rs
+++ crates/sui-sdk/src/lib.rs
@@ -77,6 +77,7 @@ use std::time::Duration;
 
 use async_trait::async_trait;
 use base64::Engine;
+use ipc_client::IpcClient;
 use jsonrpsee::core::client::ClientT;
 use jsonrpsee::http_client::{HeaderMap, HeaderValue, HttpClient, HttpClientBuilder};
 use jsonrpsee::rpc_params;
@@ -102,6 +103,7 @@ use crate::error::{Error, SuiRpcResult};
 
 pub mod apis;
 pub mod error;
+mod ipc_client;
 pub mod json_rpc_error;
 pub mod sui_client_config;
 pub mod verify_personal_message_signature;
@@ -144,6 +146,8 @@ pub struct SuiClientBuilder {
     ws_url: Option<String>,
     ws_ping_interval: Option<Duration>,
     basic_auth: Option<(String, String)>,
+    ipc_path: Option<String>,
+    ipc_pool_size: usize,
 }
 
 impl Default for SuiClientBuilder {
@@ -154,6 +158,8 @@ impl Default for SuiClientBuilder {
             ws_url: None,
             ws_ping_interval: None,
             basic_auth: None,
+            ipc_path: None,
+            ipc_pool_size: 50,
         }
     }
 }
@@ -174,6 +180,18 @@ impl SuiClientBuilder {
     /// Set the WebSocket URL for the Sui network
     pub fn ws_url(mut self, url: impl AsRef<str>) -> Self {
         self.ws_url = Some(url.as_ref().to_string());
+        self
+    }
+
+    /// Set the IPC path for the Sui network
+    pub fn ipc_path(mut self, path: impl AsRef<str>) -> Self {
+        self.ipc_path = Some(path.as_ref().to_string());
+        self
+    }
+
+    /// Set the IPC pool size
+    pub fn ipc_pool_size(mut self, pool_size: usize) -> Self {
+        self.ipc_pool_size = pool_size;
         self
     }
 
@@ -251,6 +269,16 @@ impl SuiClientBuilder {
             None
         };
 
+        let ipc = if let Some(ref ipc_path) = self.ipc_path {
+            Some(
+                IpcClient::new(ipc_path, self.ipc_pool_size)
+                    .await
+                    .map_err(|e| error::Error::IpcError(e.to_string()))?,
+            )
+        } else {
+            None
+        };
+
         let mut http_builder = HttpClientBuilder::default()
             .max_request_size(2 << 30)
             .set_headers(headers.clone())
@@ -264,7 +292,12 @@ impl SuiClientBuilder {
 
         let info = Self::get_server_info(&http, &ws).await?;
 
-        let rpc = RpcClient { http, ws, info };
+        let rpc = RpcClient {
+            http,
+            ws,
+            ipc,
+            info,
+        };
         let api = Arc::new(rpc);
         let read_api = Arc::new(ReadApi::new(api.clone()));
         let quorum_driver_api = QuorumDriverApi::new(api.clone());
@@ -475,6 +508,7 @@ pub struct SuiClient {
 pub(crate) struct RpcClient {
     http: HttpClient,
     ws: Option<WsClient>,
+    ipc: Option<IpcClient>,
     info: ServerInfo,
 }
 
