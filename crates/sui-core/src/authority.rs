325c56db57d623123377f0eeb02ff1debe88f98f
publicqi <qi@fuzz.land>
2024-12-13 16:31:32 -0800

    vis

--- crates/sui-core/src/authority.rs
+++ crates/sui-core/src/authority.rs
@@ -78,7 +78,7 @@ use tracing::{debug, error, info, instrument, warn};
 
 use self::authority_store::ExecutionLockWriteGuard;
 use self::authority_store_pruner::AuthorityStorePruningMetrics;
-pub use authority_store::{AuthorityStore, ResolverWrapper, UpdateType};
+pub use authority_store::{AuthorityStore, ResolverWrapper, SuiLockResult, UpdateType};
 use mysten_metrics::{monitored_scope, spawn_monitored_task};
 
 use crate::jsonrpc_index::IndexStore;
