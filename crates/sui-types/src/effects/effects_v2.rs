d5fa5f2f48c260da33134675abcf0ef32d655b70
Jacob Chia <jacob-chia@outlook.com>
2025-01-04 21:08:26 +0800

    gas_charger: donot panic

--- crates/sui-types/src/effects/effects_v2.rs
+++ crates/sui-types/src/effects/effects_v2.rs
@@ -498,11 +498,11 @@ impl TransactionEffectsV2 {
             .collect();
         let changed_objects: Vec<_> = changed_objects.into_iter().collect();
 
-        let gas_object_index = gas_object.map(|gas_id| {
+        let gas_object_index = gas_object.and_then(|gas_id| {
             changed_objects
                 .iter()
                 .position(|(id, _)| id == &gas_id)
-                .unwrap() as u32
+                .map(|pos| pos as u32)
         });
 
         let result = Self {
